e5ed05721db32fd566957a85accf20c5419ad53e
Amronn <zarkadiusz18@gmail.com>
2025-04-13 15:43:07 +0200

    updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,24 @@
 #include <Arduino.h>
+#include <FastLED.h>
+
+
+#define NUM_LEDS 176
+CRGB leds[NUM_LEDS];
+
+#define DATA_PIN 3
+#define BRIGHTNESS 255
+
+void setup() {
+    FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);  // GRB ordering is assumed
+}
+
+void loop() {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB::Red;
+    FastLED.show();
+    delay(100);
+    leds[i] = CRGB::Black;
+  }
+}
+
+
