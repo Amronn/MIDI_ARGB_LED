6f60f4a133011c98397b3ac7d74f90d4e29afccc
Amronn <zarkadiusz18@gmail.com>
2025-04-18 19:17:38 +0200

    added midi callback

--- src/main.cpp
+++ src/main.cpp
@@ -10,17 +10,27 @@ CRGB leds[NUM_LEDS];
 #define DATA_PIN 3
 #define BRIGHTNESS 255
 
+// Custom MIDI callback that prints incoming messages.
+struct MyMIDI_Callbacks : MIDI_Callbacks {
+
+  // Callback for channel messages (notes, control change, pitch bend, etc.).
+  void onChannelMessage(MIDI_Interface &, ChannelMessage cm) override {
+    // Print the message
+    Serial << F("Channel message: ")                                        //
+           << hex << cm.header << ' ' << cm.data1 << ' ' << cm.data2 << dec //
+           << F(" on cable ") << cm.cable.getOneBased() << endl;
+  }
+} callback;
+
 void setup() {
-    FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
+  Serial.begin(115200);        // For printing the messages
+  midi.begin();                // Initialize the MIDI interface
+  midi.setCallbacks(callback); // Attach the custom callback
 }
 
 void loop() {
-  for (int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = CRGB::Red;
-    FastLED.show();
-    delay(100);
-    leds[i] = CRGB::Black;
-  }
+  midi.update(); // Continuously handle MIDI input
 }
 
 
